53b5ed09f50ae52cf8b238fdc0ebbfa52d4f63d2
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-05-09 23:48:24 +0530

    save id in local storage

--- js/FavMovie.js
+++ js/FavMovie.js
@@ -2,139 +2,41 @@ const favMoviesContainer = document.getElementById("fav-movies-container");
 const showFavourites = document.getElementById("favorites-section");
 const emptyFavText = document.getElementById("empty-fav-text");
 
-console.log("a", window.location);
-const pageHref = window.location.search;
-// Construct a new object and pass the page href to URLSearchParams
-const searchParams = new URLSearchParams(pageHref);
-const movieID = searchParams.get("i");
-console.log("i = ", movieID);
+// console.log("a", window.location);
+// const pageHref = window.location.search;
+// // Construct a new object and pass the page href to URLSearchParams
+// const searchParams = new URLSearchParams(pageHref);
+// const movieID = searchParams.get("i");
+// console.log("i = ", movieID);
 
-addToFavDOM();
-showEmptyText();
-
-let favMovieArray = [];
-
-function showEmptyText() {
-  if (favMoviesContainer.innerHTML == "") {
-    emptyFavText.style.display = "block";
-  } else {
-    emptyFavText.style.display = "none";
-  }
-}
-  
- // Fetches data from api and calls function to add it in
- async function fetchMoviesByID() {
-  const url = `https://www.omdbapi.com/?apikey=e8e9899f&i=${movieID}`;
+// Retrieve imdbID from local storage
+let imdbID = localStorage.getItem("imdbID");
+console.log("i = ", imdbID);
+// Fetch data for the clicked movie
+async function fetchMovieData(imdbID) {
+  const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=e8e9899f`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-    return data;
+  // Display the fetched data on the page as required
+  // ...
 }
 
+fetchMovieData(imdbID);
 
-// Add to favourite of localStorage
-async function handleFavBtn(e) {
-  const target = e.target;
-  console.log(target);
-  let data = await fetchMoviesByID(target);
-  console.log(data)
-  let favMoviesLocal = localStorage.getItem("favMoviesList");
+// // Get the query parameters from the URL
+// const urlParams = new URLSearchParams(window.location.search);
 
-  if (favMoviesLocal) {
-    favMovieArray = Array.from(JSON.parse(favMoviesLocal));
-  } else {
-    localStorage.setItem("favMoviesList", JSON.stringify(data));
-  }
+// // Get the movie data from the query parameters
+// const imdbID = urlParams.get("i");
+// const title = urlParams.get("title");
+// const year = urlParams.get("year");
+// const poster = urlParams.get("poster");
 
-  // to check if movie is already present in the fav list
-  let isPresent = false;
-  favMovieArray.forEach((movie) => {
-    if (data.Title == movie.Title) {
-      notify("already added to fav list");
-      isPresent = true;
-    }
-  });
+// // Display the movie information
+// const movieTitle = document.getElementById("movie-title");
+// const movieYear = document.getElementById("movie-year");
+// const moviePoster = document.getElementById("movie-poster");
 
-  if (!isPresent) {
-    favMovieArray.push(data);
-  }
-
-  localStorage.setItem("favMoviesList", JSON.stringify(favMovieArray));
-  isPresent = !isPresent;
-  addToFavDOM();
-}
-
-// Add to favourite list DOM
-function addToFavDOM() {
-  favMoviesContainer.innerHTML = "";
-
-  let favList = JSON.parse(localStorage.getItem("favMoviesList"));
-  if (favList) {
-    favList.forEach((movie) => {
-      const div = document.createElement("div");
-
-      div.innerHTML = ` 
-    <img
-      src="${movie.Poster}"
-      alt=""
-      class="fav-movie-poster"
-    />
-    <div class="movie-card-details">
-      <p class="movie-name mt-3 mb-0">
-       <a href = "./MovieInfo/MovieInfo.html" class="fav-movie-name" data-id="${movie.Title}">${movie.Title}<a> 
-      </p>
-      <small class="text-muted">${movie.Year}</small>
-    </div>
-    <div class="delete-btn my-4">
-        <i class="fa-solid fa-trash-can" data-id="${movie.Title}"></i>
-    </div>
-    `;
-      favMoviesContainer.prepend(div);
-    });
-  }
-}
-
-// To notify
-function notify(text) {
-  window.alert(text);
-}
-
-// Delete from favourite list
-function deleteMovie(name) {
-  let favList = JSON.parse(localStorage.getItem("favMoviesList"));
-  let updatedList = Array.from(favList).filter((movie) => {
-    return movie.Title != name;
-  });
-
-  localStorage.setItem("favMoviesList", JSON.stringify(updatedList));
-
-  addToFavDOM();
-  showEmptyText();
-}
-
-// Handles click events
-async function handleClickListner(e) {
-  const target = e.target;
-
-  if (target.classList.contains("add-fav")) {
-    e.preventDefault();
-    handleFavBtn(e);
-  } else if (target.classList.contains("fa-trash-can")) {
-    deleteMovie(target.dataset.id);
-  } else if (target.classList.contains("fa-bars")) {
-    if (showFavourites.style.display == "flex") {
-      document.getElementById("show-favourites").style.color = "#8b9595";
-      showFavourites.style.display = "none";
-    } else {
-      showFavourites.classList.add("animate__backInRight");
-      document.getElementById("show-favourites").style.color =
-        "var(--logo-color)";
-      showFavourites.style.display = "flex";
-    }
-  }
-
-  localStorage.setItem("movieName", target.dataset.id);
-  // Event listner on whole document
-  document.addEventListener("click", handleClickListner);
-}
-
+// movieTitle.textContent = title;
+// movieYear.textContent = year;
+// moviePoster.setAttribute("src", poster);
--- js/script.js
+++ js/script.js
@@ -29,26 +29,32 @@ async function fetchMovies(searchTerm) {
 
 // Display search movies list
 function displayMovieList(movies) {
-  // console.log("displayMovie: ", movies);
   movies.map((item) => {
     let movieListItem = document.createElement("div");
     movieListItem.classList.add("search-list-item");
     movieListItem.innerHTML = `
       <div class="search-item-container">
-        <a href="MovieInfo.html?i=${item.imdbID}"> 
+        <a href="MovieInfo.html?i=${item.imdbID}">
           <div class = "search-item">
-            <img src = "${(item.Poster != "N/A") ? item.Poster : "../image/not-found.png"}">
-            <div clas="">
-            <h4>${item.Title}</h4>
-            ${item.Year}</div>
+            <img src="${(item.Poster != "N/A") ? item.Poster : "../image/not-found.png"}">
+            <div class="">
+              <h4>${item.Title}</h4>
+              ${item.Year}
+            </div>
           </div>
-          </a>
-          <a href="FavMovie.html?i=${item.imdbID}"> 
-          <button class="btn btn-info" type="submit"">Add</button>
-          </a>
+        </a>
+        <button class="btn btn-info add-btn" data-imdbid="${item.imdbID}" type="submit">Add</button>
       </div>
-        `;
+    `;
     suggestionsContainer.appendChild(movieListItem);
+
+    // Add click event listener to "Add" button
+    let addBtn = movieListItem.querySelector(".add-btn");
+    addBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      let imdbID = event.target.dataset.imdbid;
+      localStorage.setItem("imdbID", imdbID);
+      window.location.href = "FavMovie.html";
+    });
   });
 }
-
